8ef317612a06f1c9901502e282214123bdfcd8e8
Joao Lucas <luucaas.o.p@gmail.com>
2020-05-21 23:55:28 -0300

    Ajustando stage build

--- build/alpine.sh
+++ build/alpine.sh
@@ -5,6 +5,7 @@ apk update && \
 	apk add build-base \
     libxml2-dev \
     libxslt-dev \
+    curl \
     # postgresql-dev \
     curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
     && rm -rf /var/cache/apk/*
