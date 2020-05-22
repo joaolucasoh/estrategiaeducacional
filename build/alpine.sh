49fb854414499c3101a2d1519e1d88272ea5a423
Joao Lucas <luucaas.o.p@gmail.com>
2020-05-22 00:03:36 -0300

    Ajustando stage build

--- build/alpine.sh
+++ build/alpine.sh
@@ -1,12 +1,13 @@
 echo "http://dl-4.alpinelinux.org/alpine/v3.9/main" >> /etc/apk/repositories && \
 echo "http://dl-4.alpinelinux.org/alpine/v3.9/community" >> /etc/apk/repositories
 
-apk update && \
-	apk add build-base \
-    curl curl-dev bash \
+apk add --update --no-cache \
+    build-base \
+    mysql \
+    mysql-dev \
+    mysql-client \
+    tzdata \
     libxml2-dev \
     libxslt-dev \
-    curl \
-    # postgresql-dev \
-    curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
-    && rm -rf /var/cache/apk/*
+    curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont \
+    rm -rf /var/cache/apk/*
